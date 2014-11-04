b0dacf8e013423b32d54f195c797b0b2d044a235
Andrey Ershov <andrershov@gmail.com>
2014-11-04 19:54:38 +0400

    Added different genetic code command option to exonerate_parallel.sh

--- scripts/exonerate_parallel.sh
+++ scripts/exonerate_parallel.sh
@@ -5,7 +5,7 @@ echo "Running $1 exonerate instances"
 for i in $(seq 1 1 $1)
 do
    echo $i
-   exonerate --model protein2genome -t ../data/T_thermophila_June2014_assembly.fasta -q ../data/sample-proteins.fa --percent 80 --showtargetgff --showvulgar no --showalignment no --querychunkid $i --querychunktotal $1] > ../data/exout_$i &
+   exonerate --model protein2genome -t ../data/T_thermophila_June2014_assembly.fasta -q /home/andrey/bionif/bi2014-tetrahymena-thermophilia/data/genomes/relatives/proteins_part.fast --percent 80 --showtargetgff --showvulgar no --showalignment no --geneticcode 6 --querychunkid $i --querychunktotal $1] > ../data/exout_$i &
    pids+=($!)
 done
 
